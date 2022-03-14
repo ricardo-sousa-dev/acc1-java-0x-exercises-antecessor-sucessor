b3058a296c177a72e8bf34e476b62457823aaad6
Rafael Martins <rafamarts@gmail.com>
2022-03-14 19:15:50 -0300

    Arrumando checkstyle

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class AntecessorSucessor {
 
-	public static void main(String[] args) {	
-		// Ler a entrada de um número pelo console	
-		Scanner scan = new Scanner(System.in);
+  /**
+   * Método inicial.
+   * 
+   */
+  public static void main(String[] args) {
+    // Ler a entrada de um número pelo console
+    Scanner scan = new Scanner(System.in);
 
-		System.out.println("Digite um número:");
-		int numero = scan.nextInt();
-		
-		// Seu código aqui
-				
-		// Imprime a saída no console. OBS: descomente a próxima linha
-		//System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
+    System.out.println("Digite um número:");
+    int numero = scan.nextInt();
 
-	}
+    // Seu código aqui
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    // System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " +
+    // sucessor + " como seu sucessor.");
+
+  }
 
 }
