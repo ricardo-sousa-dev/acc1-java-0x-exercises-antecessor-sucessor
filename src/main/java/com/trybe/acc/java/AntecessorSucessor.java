8c58370f6abc59973bcb0cd960f2507e912749b4
ricardo-sousa <rwmsousa@gmail.com>
2022-03-16 17:53:33 -0300

    fix: formmat code

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -9,17 +9,19 @@ public class AntecessorSucessor {
    * 
    */
   public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-  
-      System.out.println("Digite um número:");
-      int numero = scan.nextInt(); // lê um inteiro: https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
-      int antecessor = numero -1;
-      int sucessor = numero +1;
+    Scanner scan = new Scanner(System.in);
 
-      String novaString = "O número %s tem %s como seu antecessor e %s como seu sucessor.";
-      System.out.format(novaString, numero, antecessor, sucessor); //https://docs.oracle.com/javase/tutorial/java/data/numberformat.html
-   
-      scan.close(); // Fecha o Scanner: https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
+    System.out.println("Digite um número:");
+    int numero = scan.nextInt(); // lê um inteiro:
+                                 // https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
+    int antecessor = numero - 1;
+    int sucessor = numero + 1;
+
+    String novaString = "O número %s tem %s como seu antecessor e %s como seu sucessor.";
+    System.out.format(novaString, numero, antecessor, sucessor); // https://docs.oracle.com/javase/tutorial/java/data/numberformat.html
+
+    scan.close(); // Fecha o Scanner:
+                  // https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
   }
 
 }
