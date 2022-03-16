6ad1c77ce0f71497a1bb7d369ff81f9d3e6327fa
ricardo-sousa <rwmsousa@gmail.com>
2022-03-16 18:13:49 -0300

    fix: formmat code

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -12,16 +12,19 @@ public class AntecessorSucessor {
     Scanner scan = new Scanner(System.in);
 
     System.out.println("Digite um número:");
-    int numero = scan.nextInt(); // lê um inteiro:
-                                 // https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
+    // lê um inteiro:
+    // https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
+    int numero = scan.nextInt();
     int antecessor = numero - 1;
     int sucessor = numero + 1;
 
     String novaString = "O número %s tem %s como seu antecessor e %s como seu sucessor.";
-    System.out.format(novaString, numero, antecessor, sucessor); // https://docs.oracle.com/javase/tutorial/java/data/numberformat.html
+    System.out.format(novaString, numero, antecessor, sucessor);
+    // https://docs.oracle.com/javase/tutorial/java/data/numberformat.html
 
-    scan.close(); // Fecha o Scanner:
-                  // https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
+    // Fecha o Scanner:
+    scan.close();
+    // https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
   }
 
 }
